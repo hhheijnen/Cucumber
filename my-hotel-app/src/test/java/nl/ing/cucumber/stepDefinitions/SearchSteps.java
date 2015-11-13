5c7aeee62aa24a31b1761c77646932f4b5aa90c7
nl25029 <nl25029@NLCNU1210NTC.ww930.my-it-solutions.net>
2015-11-13 10:31:26 +0100

    Added jar for EMS communication

--- my-hotel-app/src/test/java/nl/ing/cucumber/stepDefinitions/SearchSteps.java
+++ my-hotel-app/src/test/java/nl/ing/cucumber/stepDefinitions/SearchSteps.java
@@ -1,11 +1,5 @@
 package nl.ing.cucumber.stepDefinitions;
 
-import java.util.List;
-
-import org.junit.Assert;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
-
 import cucumber.api.java.en.Given;
 import cucumber.api.java.en.Then;
 import cucumber.api.java.en.When;
